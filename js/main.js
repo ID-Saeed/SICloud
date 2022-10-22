3572b2b4a407993aed48aa44d94b5ef0acec508f
ID-Saeed <112485076+ID-Saeed@users.noreply.github.com>
2022-10-22 16:04:38 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let langs = document.querySelectorAll(".langs select option");
+langs.forEach(ele =>
+{
+    // console.log(ele);
+    // ele.addEventListener("cuechange", function ()
+    // {
+    // });
+    // if (ele.value == "AR") {
+    //     console.log("Arbic");
+    // } else {
+    //     console.log("English");
+    // }
+});
